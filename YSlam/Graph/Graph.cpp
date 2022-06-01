15a2b96560e0171c3817b9b817793f391165617b
Dan <ysjeung@maxst.com>
2022-06-01 21:11:53 +0900

    그래프 어케 만들지...

--- YSlam/Graph/Graph.cpp
+++ YSlam/Graph/Graph.cpp
@@ -3,6 +3,22 @@
 namespace dan {
 
 Graph::Graph() {}
-Graph::~Graph() {}
+Graph::~Graph() {
+
+	for (int i = 0; i < frames.size(); i++) {
+		delete frames[i];
+	}
+	for (int i = 0; i < mapPoints.size(); i++) {
+		delete mapPoints[i];
+	}
+
+}
+
+void Graph::addFactor(Frame* frame, MapPoint* mapPoint) {
+	frames.push_back(frame);
+	mapPoints.push_back(mapPoint);
+
+}
+
 
 }
--- YSlam/Graph/Graph.h
+++ YSlam/Graph/Graph.h
@@ -1,11 +1,28 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 #include <list>
 #include <map>
 #include "Parameters/Parameters.h"
 
 namespace dan {
+class Frame;
+class MapPoint;
+class Factor {
+public:
+
+	Factor() {}
+	~Factor() {}
+
+	Frame* frame;
+	MapPoint* mapPoint;
+
+private:
+
+};
+
+
 
 class Frame;
 class MapPoint;
@@ -15,9 +32,18 @@ public:
 
 	Graph();
 	~Graph();
+	void addFactor(Frame* frame, MapPoint* mapPoint);
 
-	std::list<Frame*> frames;
-	std::map<int, MapPoint*> mapPoints;
+	//node
+	std::vector<Frame*> frames;
+	std::vector<MapPoint*> mapPoints;
+	
+	std::vector<Factor*> factors;
+
+	//factor
+	std::map<Frame, std::vector<Factor*>> kfFactors;
+	std::map<MapPoint*, std::vector<Factor*>> mpFactor;
+
 private:
 
 
